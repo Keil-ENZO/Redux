dfa92248f5ce5c6c22c020e8448ce2869b55545e
Keil-ENZO <enzo.keil06@icloud.com>
2023-10-24 11:53:50 +0200

    post

--- src/actions/post.action.js
+++ src/actions/post.action.js
@@ -1,12 +1,25 @@
 import axios from "axios";
 
 export const GET_POSTS = "GET_POST";
+export const ADD_POST = "ADD_POST";
 
 export const getPost = () => {
     return (dispatch) => {
         return axios.get("http://localhost:3000/posts")
             .then((res) => {
                 dispatch({ type: GET_POSTS , payload: res.data })
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+    }
+}
+
+export const addPost = (data) => {
+    return (dispatch) => {
+        return axios.post("http://localhost:3000/posts", data)
+            .then((res) => {
+                dispatch({ type: ADD_POST , payload: data })
             })
             .catch((error) => {
                 console.log(error);
--- src/reducers/post.reducer.js
+++ src/reducers/post.reducer.js
@@ -8,6 +8,9 @@ export default function postReducer(state = initialState, action) {
     switch (action.type) {
         case GET_POSTS:
             return action.payload;
+
+            case "ADD_POST":
+                return [action.payload, ...state]
         default:
             return state;
     }
